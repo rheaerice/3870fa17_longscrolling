9ac9c7be0894c6b5e53fb3e316cd793c87157750
rheaerice <rerice@horizon.csueastbay.edu>
2017-11-28 18:02:43 -0800

    fixed items ready

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@
 
 
 var canvas = document.getElementById("canvas");
+var bowl = document.getElementById("bowl");
 var avocado = document.getElementById("avocado");
+var prep = document.getElementById("prep");
+var ingredients = document.getElementById("ingredients");
+
 
 
 
@@ -10,9 +14,16 @@ window.onscroll=function(){
 var y = window.pageYOffset;
 console.log(y);
 
+/*bowl*/
+bowl.style.top=200 + y * 1.1 + "px";
+
+
+ /*side bars*/
+ prep.style.top=200 + y * 1.1 + "px";
+ ingredients.style.top=200 + y * 1.1 + "px";
 
 /*avocado*/
-avocado.style.top=200 + y * 1.1 + "px";
+avocado.style.top=1000 + y * .7 + "px";
  if (y > 2000)
 {avocado.src ="http://www.shockinglydelicious.com/wp-content/uploads/2011/03/Avocado-flesh-in-a-bowl.jpg";
 	avocado.style.transform="translate(0,0)";
